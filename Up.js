fd7ec1d7e3247a4ad9b365dbb97f6ec869165d0a
20valera05 <20valera05@gmail.com>
2024-05-29 20:57:05 +0300

    first commit

---
+++ Up.js
@@ -0,0 +1,40 @@
+// document.getElementById("wordInput").addEventListener("input", function(){
+// let wordContainer = document.getElementById("wordContainer");
+// wordContainer.innerHTML = wordInput;
+// })
+
+
+let wordInput = document.getElementById("wordInput");
+let wordContainer = document.getElementById("wordContainer");
+
+wordInput.addEventListener("keydown", function () {
+    if (event.keyCode === 13) {
+        wordContainer.innerHTML = "";
+
+
+        let letters = wordInput.value;
+        wordInput.value = "";
+        for (let i = 0; i < letters.length; i++) {
+            let char = letters[i];
+            let wordSquare = document.createElement("div");
+            wordSquare.classList.add("wordSquare");
+
+            let frontContainer = document.createElement("div");
+            frontContainer.classList.add("frontContainer");
+
+            let backContainer = document.createElement("div")
+            backContainer.classList.add("backContainer");
+            backContainer.textContent = char;
+
+            wordSquare.appendChild(frontContainer);
+            wordSquare.appendChild(backContainer);
+
+            wordContainer.appendChild(wordSquare);
+
+            wordSquare.addEventListener("click", function () {
+                this.classList.toggle("flipped")
+            })
+        }
+    }
+
+})
